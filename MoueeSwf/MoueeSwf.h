25f1971e2e68480f088b6ddbac1114a49618a726
加帆 <jiafan.zhang@qq.com>
2015-09-07 11:57:34 +0800

    添加源文件

---
+++ MoueeSwf/MoueeSwf.h
@@ -0,0 +1,21 @@
+//
+//  MoueeSwf.h
+//  MoueeSwf
+//
+//  Created by Jiafan on 9/7/15.
+//  Copyright (c) 2015 Jiafan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MoueeSwf.
+FOUNDATION_EXPORT double MoueeSwfVersionNumber;
+
+//! Project version string for MoueeSwf.
+FOUNDATION_EXPORT const unsigned char MoueeSwfVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MoueeSwf/PublicHeader.h>
+#import <MoueeSwf/SwiffMovie.h>
+#import <MoueeSwf/SwiffView.h>
+
+
